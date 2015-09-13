752684c4a582476d752ed15890156fc2001d691f
Depado <paul.lhussiez@gmail.com>
2015-09-13 16:17:46 +0200

    Updating comments

--- plugins/commands/example/example.go
+++ plugins/commands/example/example.go
@@ -12,6 +12,8 @@ import "github.com/thoj/go-ircevent"
 
 // Plugin is the plugin struct. It will be exposed as packagename.Plugin to keep the API stable and friendly.
 // It also makes sure you don't have two plugins coming from the same package.
+// This struct can also be used to save a state that will persist across multiple commands.
+// For a more complex example, see the urban plugin.
 type Plugin struct{}
 
 // Help must send some help about what the command actually does and how to call it if there are any optional arguments.
@@ -25,6 +27,7 @@ func (p Plugin) Help(ib *irc.Connection, from string) {
 // from: The nickname which executed the command. Useful to highlight said person or send a private message.
 // to: The channel in which the command has been executed. You can determine if the command has been sent directly to the bot in a private message for example.
 // args: An array of strings, representing everything after the command call.
+// Note : If you intend to modify the state of the plugin, (p Plugin) should then become (p *Plugin)
 func (p Plugin) Get(ib *irc.Connection, from string, to string, args []string) {
 	ib.Privmsg(to, "This command doesn't do much, but I'm glad you tried it out.")
 }
