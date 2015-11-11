3c23ec1a14bb433edb603540753fc4a2e3d466a3
Depado <paul.lhussiez@gmail.com>
2015-11-11 16:40:48 +0100

    Fixing again c:

--- utils/logging.go
+++ utils/logging.go
@@ -22,30 +22,29 @@ var HistoryLogger *log.Logger
 var HistoryFile *os.File
 
 // InitLoggers initialize the loggers to use with the logger middleware.
-func InitLoggers() (err error) {
+func InitLoggers() error {
+	var err error
 	sc := strings.Replace(configuration.Config.Channel, "#", "", 1)
-	err = CheckAndCreateFolder("logs")
-	if err != nil {
-		return
+	if err = CheckAndCreateFolder("logs"); err != nil {
+		return err
 	}
 	lf := path.Join("logs", sc)
-	err = CheckAndCreateFolder(lf)
-	if err != nil {
-		return
+	if err = CheckAndCreateFolder(lf); err != nil {
+		return err
 	}
 
 	HistoryFile, err := os.OpenFile(path.Join(lf, "history.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return
+		return err
 	}
 
 	LinkFile, err := os.OpenFile(path.Join(lf, "links.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
-		return
+		return err
 	}
 
 	LinkLogger = log.New(LinkFile, "", log.Ldate|log.Ltime)
 	HistoryLogger = log.New(HistoryFile, "", log.Ldate|log.Ltime)
 
-	return
+	return err
 }
