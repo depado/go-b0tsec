cee6522987980615134b4a7742a7df34c9651339
Depado <paul.lhussiez@gmail.com>
2015-11-11 16:21:40 +0100

    Fixing

--- utils/logging.go
+++ utils/logging.go
@@ -12,13 +12,14 @@ import (
 // LinkLogger is used to aggregate links.
 var LinkLogger *log.Logger
 
+// LinkFile is the file associated to the LinkLogger
+var LinkFile *os.File
+
 //HistoryLogger is used to aggregate all the messages.
 var HistoryLogger *log.Logger
 
-var (
-	linkFile    *os.File
-	historyFile *os.File
-)
+// HistoryFile is the file associated to the HistoryLogger
+var HistoryFile *os.File
 
 // InitLoggers initialize the loggers to use with the logger middleware.
 func InitLoggers() (err error) {
@@ -33,18 +34,18 @@ func InitLoggers() (err error) {
 		return
 	}
 
-	historyFile, err := os.OpenFile(path.Join(lf, "history.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	HistoryFile, err := os.OpenFile(path.Join(lf, "history.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return
 	}
 
-	linkFile, err := os.OpenFile(path.Join(lf, "links.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
+	LinkFile, err := os.OpenFile(path.Join(lf, "links.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		return
 	}
 
-	LinkLogger = log.New(linkFile, "", log.Ldate|log.Ltime)
-	HistoryLogger = log.New(historyFile, "", log.Ldate|log.Ltime)
+	LinkLogger = log.New(LinkFile, "", log.Ldate|log.Ltime)
+	HistoryLogger = log.New(HistoryFile, "", log.Ldate|log.Ltime)
 
 	return
 }
